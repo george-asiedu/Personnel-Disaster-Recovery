1e77ae2a7f4f06c63e01bc2de0658d67b43a0239
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-04 15:35:04 +0000

    profile form updated to use custom validators

--- src/app/personnel/profile-form/profile-form.component.ts
+++ src/app/personnel/profile-form/profile-form.component.ts
@@ -5,6 +5,9 @@ import { Router } from '@angular/router';
 import { NgToastService } from 'ng-angular-popup';
 import { SpinnerComponent } from '../../components/spinner/spinner.component';
 import { Profile } from '../../model/profile';
+import { UserDataService } from '../../shared/user-data/user-data.service';
+import { phoneNumberValidator } from '../../validators/phoneNumberValidator';
+import { PhoneNumberDirective } from '../../directive/phone-number/phone-number.directive';
 
 @Component({
   selector: 'app-profile-form',
@@ -15,7 +18,8 @@ import { Profile } from '../../model/profile';
     NgSwitch, 
     NgSwitchCase, 
     NgIf,
-    SpinnerComponent
+    SpinnerComponent,
+    PhoneNumberDirective
   ],
   templateUrl: './profile-form.component.html',
   styleUrl: './profile-form.component.scss'
@@ -28,35 +32,35 @@ export class ProfileFormComponent {
   constructor(
     private fb: FormBuilder,
     private toast: NgToastService,
-    private router: Router
-  ) {
-    this.profileForm = this.fb.group({
+    private router: Router,
+    private userService: UserDataService) {
+      this.profileForm = this.fb.group({
       
-      name: ['', Validators.required],
-      dob: ['', Validators.required],
-      gender: ['', Validators.required],
-      phoneNumber: ['', [Validators.required, Validators.pattern(/^[0-9]{10}$/)]],
-      houseAddress: ['', Validators.required],
-      photograph: ['', Validators.required],
+        name: [{ value: this.userService.getUser()?.name, disabled: true }],
+        dob: ['', Validators.required],
+        gender: ['', Validators.required],
+        phoneNumber: ['', [Validators.required, phoneNumberValidator()]],
+        houseAddress: ['', Validators.required],
+        photograph: ['', Validators.required],
 
-      jobTitle: ['', Validators.required],
-      department: ['', Validators.required],
-      employeeId: ['', Validators.required],
-      currentPosition: ['', Validators.required],
-      yearsOfExperience: ['', Validators.required],
-      currentWorkplace: ['', Validators.required],
-      employerEmail: ['', [Validators.required, Validators.email]],
+        jobTitle: ['', Validators.required],
+        department: ['', Validators.required],
+        employeeId: ['', Validators.required],
+        currentPosition: ['', Validators.required],
+        yearsOfExperience: ['', Validators.required],
+        currentWorkplace: ['', Validators.required],
+        employerEmail: ['', [Validators.required, Validators.email]],
 
-      qualification: ['', Validators.required],
-      fieldOfStudy: ['', Validators.required],
+        qualification: ['', Validators.required],
+        fieldOfStudy: ['', Validators.required],
 
-      areaOfExpertise: ['', Validators.required],
-      skills: ['', Validators.required],
-      projectsUndertaken: ['', Validators.required],
+        areaOfExpertise: ['', Validators.required],
+        skills: ['', Validators.required],
+        projectsUndertaken: ['', Validators.required],
 
-      languageSpoken: ['', Validators.required],
-      hobbies: ['', Validators.required],
-      interest: ['', Validators.required]
+        languageSpoken: ['', Validators.required],
+        hobbies: ['', Validators.required],
+        interest: ['', Validators.required]
     })
   }
 
