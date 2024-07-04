1f69900ca1fd7d0cc4b799d453e13f650a8516ec
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-04 15:33:58 +0000

    navbar component updated to get the name initials and store in local storage

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserDataService } from '../../shared/user-data/user-data.service';
+import { LoginResponse } from '../../model/login';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  userInitials: string = ''
 
+  constructor(private userService: UserDataService) {}
+
+  ngOnInit(): void {
+      this.userService.user$.subscribe(
+        (user: LoginResponse['data']['user'] | null) => {
+          if(user) {
+            this.userInitials = this.getInitials(user.name)
+          }
+        }
+      )
+  }
+
+  getInitials(name: string): string {
+    const [firstName, lastName] = name.split(' ')
+    const initials = firstName.charAt(0).toUpperCase() + (lastName ? lastName.charAt(0).toUpperCase() : '')
+    return initials
+  }
 }
