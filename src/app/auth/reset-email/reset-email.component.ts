958896e2b39f6c93bbc165d6afa2bf04c74c00e1
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-05 11:43:24 +0000

    reset email component created

---
+++ src/app/auth/reset-email/reset-email.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-email',
+  standalone: true,
+  imports: [],
+  templateUrl: './reset-email.component.html',
+  styleUrl: './reset-email.component.scss'
+})
+export class ResetEmailComponent {
+
+}
