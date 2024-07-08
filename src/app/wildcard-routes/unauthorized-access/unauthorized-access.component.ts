aac97b3cb5e57f99b205b734bd22586ed298b9d6
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-08 13:55:13 +0000

    update

--- src/app/wildcard-routes/unauthorized-access/unauthorized-access.component.ts
+++ src/app/wildcard-routes/unauthorized-access/unauthorized-access.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-unauthorized-access',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './unauthorized-access.component.html',
   styleUrl: './unauthorized-access.component.scss'
 })
