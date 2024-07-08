891958fb7627ab0ec30520498743ac9486e94811
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-08 13:56:12 +0000

    score column added

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -11,15 +11,15 @@ import { Router, RouterLink } from '@angular/router';
 })
 export class TableComponent {
   personnels = [
-    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', status: 'Pending' },
-    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', status: 'Verified' },
-    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', status: 'Pending' },
-    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', status: 'Verified' },
-    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', status: 'Pending' },
-    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', status: 'Verified' },
-    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', status: 'Pending' },
-    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', status: 'Verified' },
-    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', status: 'Suspend' }
+    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', score: 3, status: 'Pending' },
+    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', score: 1, status: 'Verified' },
+    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', score: 2, status: 'Pending' },
+    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', score: 2, status: 'Verified' },
+    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', score: 3, status: 'Pending' },
+    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', score: 2, status: 'Verified' },
+    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', score: 1, status: 'Pending' },
+    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', score: 1, status: 'Verified' },
+    { name: 'George Asiedu', email: 'asiedug41@gmail.com', date: 'May 9, 2024', title: 'Developer', score: 2, status: 'Suspend' }
   ];
 
   isDropdownVisible: boolean[] = Array(this.personnels.length).fill(false);
