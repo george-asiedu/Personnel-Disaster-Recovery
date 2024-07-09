827d43e9c50db95677194859ed7671ab517a971a
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-09 18:25:24 +0000

    auth guard created

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,19 +1,57 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, CanMatchFn, CanDeactivateFn } from '@angular/router';
 import { AuthService } from '../auth/authService/auth.service';
 import { Router } from '@angular/router';
 import { Role } from '../model/login';
+import { NgToastService } from 'ng-angular-popup';
+import { ProfileFormComponent } from '../personnel/profile-form/profile-form.component';
 
-export const authGuard: CanActivateFn = (route) => {
-  const authService = inject(AuthService)
-  const router = inject(Router)
-  const requiredRoles = route.data['roles'] as Array<Role>
+const getDependencies = () => {
+  return {
+    authService: inject(AuthService),
+    router: inject(Router),
+    toast: inject(NgToastService)
+  }
+}
+
+const handleUnauthorizedAccess = (router: Router, toast: NgToastService) => {
+  toast.warning("You don't have permission", "Access denied", 3000)
+  setTimeout(() => {
+    router.navigate(['/login'])
+  }, 3000)
+}
+
+export const canActivate: CanActivateFn = (route) => {
+  const { authService, router, toast } = getDependencies()
+  const requiredRoles = route.data?.['roles'] as Array<Role> | undefined
   const user = authService.currentUserValue?.data.user
 
-  if (user && requiredRoles.includes(user.role)) {
+  if (user && requiredRoles && requiredRoles.includes(user.role)) {
     return true
   } else {
-    router.navigate(['/unauthorized-access'])
+    handleUnauthorizedAccess(router, toast)
     return false
   }
+};
+
+export const canMatch: CanMatchFn = (route) => {
+  const { authService, router, toast } = getDependencies()
+  const requiredRoles = route.data?.['roles'] as Array<Role> | undefined
+  const user = authService.currentUserValue?.data.user
+
+  if (user && requiredRoles && requiredRoles.includes(user.role)) {
+    return true
+  } else {
+    handleUnauthorizedAccess(router, toast)
+    return false
+  }
+};
+
+export const canDeactivate: CanDeactivateFn<ProfileFormComponent> = ( component: ProfileFormComponent ) => {
+  const { toast } = getDependencies()
+  if(component.hasUnsavedChanges()) {
+    toast.warning("You have unsaved changes", "Warning", 3000)
+    return false
+  }
+  return true
 }
