4f94367c2bd1705b4398181bc12a267d244b0096
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-09 18:25:11 +0000

    navbar component updated to use dropdown

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserDataService } from '../../shared/user-data/user-data.service';
 import { LoginResponse } from '../../model/login';
 import { NgIf } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -11,15 +12,21 @@ import { NgIf } from '@angular/common';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent implements OnInit {
-  userInitials: string = ''
+  public isActive: boolean = false
+  public isDropdownOpen: boolean = false
+  public userInitials: string = ''
+  public username: string = ''
+  public email: string = ''
 
-  constructor(private userService: UserDataService) {}
+  constructor(private userService: UserDataService, private router: Router) {}
 
   ngOnInit(): void {
       this.userService.user$.subscribe(
-        (user: LoginResponse['data']['user'] | null) => {
+        (user: LoginResponse['data']['user'] | null) => {      
           if(user) {
             this.userInitials = this.getInitials(user.name)
+            this.username = user.name
+            this.email = user.email
           }
         }
       )
@@ -29,5 +36,19 @@ export class NavbarComponent implements OnInit {
     const [firstName, lastName] = name.split(' ')
     const initials = firstName.charAt(0).toUpperCase() + (lastName ? lastName.charAt(0).toUpperCase() : '')
     return initials
+  }
+
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen
+  }
+
+  accountSettings(): void {
+    this.isDropdownOpen = false
+  }
+
+  logout(): void {
+    this.userService.clearUserData()
+    this.isDropdownOpen = false
+    this.router.navigate(['/login'])
   }
 }
