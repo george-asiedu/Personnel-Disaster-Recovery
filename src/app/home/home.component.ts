1227a97343f3bbed9cd246c1d8e2760b58a1a9f2
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-10 11:49:59 +0000

    component header updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,14 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, NgClass],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  
 }
