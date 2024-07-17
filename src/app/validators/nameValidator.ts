b987cb74a12b2bb2be00cda589fd46415414a5c0
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-17 14:40:09 +0000

    regex updated

--- src/app/validators/nameValidator.ts
+++ src/app/validators/nameValidator.ts
@@ -3,7 +3,7 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms'
 export const nameValidator = (): ValidatorFn => {
   return (control: AbstractControl): ValidationErrors | null => {
     const value = control.value
-    const isValid = /^[A-Za-z\s]+$/.test(value)
+    const isValid = /^[a-zA-Z ]*$/.test(value)
     return isValid ? null : { invalidName: true }
   }
 }
