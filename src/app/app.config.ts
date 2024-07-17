979b8a5437a7249a27a98e12b88e0c2d38b54dbc
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-17 14:43:56 +0000

    interceptor added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,15 +3,16 @@ import { PreloadAllModules, provideRouter, withPreloading } from '@angular/route
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideSpinnerConfig } from "ngx-spinner";
 import { NgToastModule } from 'ng-angular-popup';
+import { authInterceptor } from './interceptor/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withPreloading(PreloadAllModules)), 
     provideAnimationsAsync(), 
-    provideHttpClient(), 
+    provideHttpClient(withInterceptors([authInterceptor])), 
     provideSpinnerConfig({ type: 'ball-clip-rotate-multiple'}),
     importProvidersFrom(NgToastModule)
   ]
