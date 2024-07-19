dced78df8bb278e7495bf9adf3079d168690ff14
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-19 11:14:39 +0000

    initiative modal updated in the component

--- src/app/admin/emergency-initiative/emergency-initiative.component.ts
+++ src/app/admin/emergency-initiative/emergency-initiative.component.ts
@@ -1,27 +1,37 @@
 import { Component } from '@angular/core';
 import { EmergencyInitiativeTableComponent } from '../../components/table/emergency-initiative-table/emergency-initiative-table.component';
 import { NgIf } from '@angular/common';
+import { CreateInitiativeModalComponent } from '../../modal/create-initiative-modal/create-initiative-modal.component';
+import { NgToastService } from 'ng-angular-popup';
+import { PaginationComponent } from '../../components/pagination/pagination.component';
 
 @Component({
   selector: 'app-emergency-initiative',
   standalone: true,
   imports: [
     EmergencyInitiativeTableComponent,
-    NgIf
+    NgIf,
+    CreateInitiativeModalComponent,
+    PaginationComponent
   ],
   templateUrl: './emergency-initiative.component.html',
   styleUrl: './emergency-initiative.component.scss'
 })
 export class EmergencyInitiativeComponent {
-  showModal(): void {
+  public isModalVisible: boolean = false
 
+  constructor(private toast: NgToastService) {}
+
+  showModal(): void {
+    this.isModalVisible = true
   }
 
   cancelModal(): void {
-
+    this.isModalVisible = false
   }
 
   onSubmit(): void {
-
+    this.isModalVisible = false
+    this.toast.success("Emergency initiated successfully", "Success", 3000)
   }
 }
