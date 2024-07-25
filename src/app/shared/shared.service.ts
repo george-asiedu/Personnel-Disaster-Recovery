29907d31b7f7f0d8b50bef10f39e47672223fd37
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-25 16:13:15 +0000

    shared service component created to allow image upload

--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -1,8 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
-import { AuthService } from '../auth/authService/auth.service';
-import { Image, ImageResponse } from '../model/image';
+import { ImageResponse } from '../model/image';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -11,21 +10,9 @@ import { Observable } from 'rxjs';
 export class SharedService {
   protected baseUrl = environment.baseUrl
 
-  constructor(private http: HttpClient, private authService: AuthService) { }
+  constructor(private http: HttpClient) {}
 
-  private get httpOptions() {
-    const accessToken = this.authService.currentUserValue?.data.token.accessToken
-    return {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`
-      })
-    }
-  }
-
-  changeImage(image: Image): Observable<ImageResponse> {
-    // const formData = new FormData()
-    // formData.append('image', image)
-    return this.http.post<ImageResponse>(`${this.baseUrl}/users/change-image`, image, this.httpOptions)
+  changeImage(formData: FormData): Observable<ImageResponse> {
+    return this.http.post<ImageResponse>(`${this.baseUrl}/users/change-image`, formData)
   }
 }
