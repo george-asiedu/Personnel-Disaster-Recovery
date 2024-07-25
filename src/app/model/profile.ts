6f75536d36c49ce1dda95668b723cf5a5b96bb95
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-25 16:09:16 +0000

    profile model defined

--- src/app/model/profile.ts
+++ src/app/model/profile.ts
@@ -1,19 +1,44 @@
 export interface Profile {
+    userId: string,
+    dob: string,
+    gender: Gender,
+    phoneNumber: string,
+    houseNumber: string,
+    town: string,
+    digitalAddress: string,
+    employeeId: string,
+    professionId: number,
+    employerName: string,
+    employerEmail: string,
+    currentPosition: Position,
+    experienceYears: number,
+    qualification: string,
+    studyField: string,
+    graduationYear: number
+}
+
+export enum Position {
+    JUNIOR = 'JUNIOR',
+    ASSOCIATE = 'ASSOCIATE',
+    EXPERT = 'EXPERT'
+}
+
+export enum Gender {
+    MALE = 'MALE',
+    FEMALE = 'FEMALE'
+}
+
+export interface GetPersonnel {
+    count: number,
+    personnel: Personnel[]
+}
+
+export interface Personnel {
     id: string,
     name: string,
-    dob: string,
-    gender: string,
-    phoneNumber: string,
-    houseAddress: string,
-    photograph: string,
-    jobTitle: string,
-    employeeId: string,
-    employeeEmail: string,
-    currentPosition: string,
-    yearsOfExperience: number,
-    currentWorkplace: string,
-    qualification: string,
-    fieldOfStudy: string,
-    areaOfExpertise: string,
-    projectsUndertaken: string,
+    email: string,
+    image: string
+    profession: string,
+    status: string,
+    createdAt: Date
 }
