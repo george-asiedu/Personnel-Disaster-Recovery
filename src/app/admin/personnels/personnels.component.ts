cc6c7acceeff8cf230265a4a9a7cb17c5d560aa3
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-25 15:58:04 +0000

    personnel component update to use count to show the total personnel value

--- src/app/admin/personnels/personnels.component.ts
+++ src/app/admin/personnels/personnels.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PersonnelTableComponent } from '../../components/table/personnel-table/personnel-table.component';
-import { PaginationComponent } from '../../components/pagination/pagination.component';
+import { PersonnelService } from '../../personnel/service/personnel.service';
 
 @Component({
   selector: 'app-personnels',
   standalone: true,
-  imports: [PersonnelTableComponent, PaginationComponent],
+  imports: [PersonnelTableComponent],
   templateUrl: './personnels.component.html',
   styleUrl: './personnels.component.scss'
 })
-export class PersonnelsComponent {
-  
+export class PersonnelsComponent implements OnInit {
+  public count: number = 0
+
+  constructor(private ps: PersonnelService) {}
+
+  ngOnInit(): void {
+    this.ps.getPersonnelData(0).subscribe(response => {
+      this.count = response.count
+    })
+  }
 }
