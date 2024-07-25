4c6211de95caacb927448e1ef65a622394c4393e
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-25 16:13:53 +0000

    phone number custom validator created

--- src/app/validators/phoneNumberValidator.ts
+++ src/app/validators/phoneNumberValidator.ts
@@ -3,7 +3,7 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms'
 export const phoneNumberValidator = (): ValidatorFn => {
   return (control: AbstractControl): ValidationErrors | null => {
     const value = control.value
-    const isValid = /^\+233[0-9]{9}$/.test(value)
+    const isValid = /^[0-9]{10}$/.test(value)
     return isValid ? null : { invalidPhoneNumber: true }
   }
 }
