10c53f8886ab43071020c241288f90e5a7f1bd95
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-25 16:00:46 +0000

    auth service updated to remove http headers

--- src/app/auth/authService/auth.service.ts
+++ src/app/auth/authService/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 import { Register } from '../../model/register';
-import { BehaviorSubject, map, Observable, switchMap, tap } from 'rxjs';
+import { BehaviorSubject, catchError, map, Observable, switchMap, tap, throwError } from 'rxjs';
 import { Login, LoginResponse, Role } from '../../model/login';
 import { ResetPassword } from '../../model/reset-password';
 import { Router } from '@angular/router';
@@ -30,59 +30,38 @@ export class AuthService {
     return value
   }
 
-  private get httpOptions() {
-    const accessToken = this.currentUserValue?.data.token.accessToken
-    return {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`
-      })
-    }
-  }
-
-  refreshToken(): Observable<LoginResponse> {
-    const refreshToken = this.currentUserValue?.data.token.refreshToken
-    return this.http.get<LoginResponse>(`${this.baseURL}/auth/token-refresh`, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${refreshToken}`
-      })
-    }).pipe(
-      tap((response: LoginResponse) => {
-        this.storeUserData(response)
-      })
-    )
-  }
-
-  private storeUserData(response: LoginResponse): void {
+  public storeUserData(response: LoginResponse): void {
     localStorage.setItem('currentUser', JSON.stringify(response))
     const storedData = this.currentUserSubject.next(response)
     return storedData
   }
 
   checkEmail(email: string): Observable<{ message: string }> {
-    return this.http.post<{ message: string }>(`${this.baseURL}/auth/check-email`, { email })
+    return this.http.post<{ message: string }>(`${this.baseURL}/auth/check-email`, {email})
   }
 
   Register(user: Register): Observable<Register> {
     return this.checkEmail(user.email).pipe(
       switchMap((response) => {
         if (response.message.includes("doesn't exist")) {
-          return this.http.post<Register>(`${this.baseURL}/auth/register`, user, this.httpOptions).pipe(
+          return this.http.post<Register>(`${this.baseURL}/auth/register`, user).pipe(
             tap(() => {
               this.router.navigate(['/verify-email'], { state: { email: user.email } })
             })
           )
         } else {
           this.toast.danger('Email already exists, try a different one', 'Error', 3000)
-          throw new Error('Email already exists, try a different one')
+          return throwError(() => new Error('Email already exists, try a different one'))
         }
+      }),
+      catchError((error) => {
+        return throwError(() => error)
       })
     )
   }
 
   Login(user: Login): Observable<LoginResponse> {
-    return this.http.post<LoginResponse>(`${this.baseURL}/auth/login`, user, this.httpOptions)
+    return this.http.post<LoginResponse>(`${this.baseURL}/auth/login`, user)
       .pipe(
         map((response: LoginResponse) => {
           this.storeUserData(response)
@@ -94,16 +73,17 @@ export class AuthService {
           }
 
           return response
-        })
+        }),
+        tap(() => {})
       )
   }
 
   submitEmail(email: string): Observable<string> {
-    return this.http.post<string>(`${this.baseURL}/auth/forgot-password`, email, this.httpOptions)
+    return this.http.post<string>(`${this.baseURL}/auth/forgot-password`, email)
   }
 
   resetPassword(token: string, reset: ResetPassword): Observable<ResetPassword> {
-    return this.http.post<ResetPassword>(`${this.baseURL}/auth/reset-password?token=${token}`, reset, this.httpOptions)
+    return this.http.post<ResetPassword>(`${this.baseURL}/auth/reset-password?token=${token}`, reset)
   }
 
   confirmEmail(token: string): Observable<{ message: string }> {
@@ -117,7 +97,7 @@ export class AuthService {
   logout(): void {
     localStorage.removeItem('currentUser')
     this.currentUserSubject.next(null)
-    this.router.navigate(['/login'])
+    this.router.navigateByUrl('/login')
   }
 
   isAuthenticated(): boolean {
@@ -127,26 +107,19 @@ export class AuthService {
   getUserRole(): Role | null {
     return this.currentUserValue?.data.user.role || null
   }
-
-  // refreshToken(): Observable<{ data: string }> {
-  //   const refreshToken = this.currentUserValue?.data.token.refreshToken
-  //   return this.http.get<{ data: string }>(`${this.baseURL}/auth/token-refresh`, {
-  //     headers: new HttpHeaders({
-  //       'Content-Type': 'application/json',
-  //       Authorization: `Bearer ${refreshToken}`
-  //     })
-  //   })
-  //   .pipe(
-  //     tap((response: { data: string }) => {
-  //       const currentUser = this.currentUserValue
-  //       console.log('current user after refresh',currentUser)
-        
-  //       if (currentUser) {
-  //         currentUser.data.token.accessToken = response.data
-  //         const refresh = this.storeUserData(currentUser)
-  //         console.log('token refresh', refresh)
-  //       }
-  //     })
-  //   )
-  // }
+  
+  tokenRefresh(): Observable<any> {
+    const refreshToken = this.currentUserValue?.data.token.refreshToken
+    const headers = new HttpHeaders({
+        Authorization: `Bearer ${refreshToken}`
+    })
+    return this.http.get<any>(`${this.baseURL}/auth/token-refresh`, {headers})
+      .pipe(
+        catchError((error) => {
+          this.toast.danger('Error refreshing token', 'Error', 3000)
+          this.logout()
+          return throwError(() => error)
+        })
+      )
+  }
 }
