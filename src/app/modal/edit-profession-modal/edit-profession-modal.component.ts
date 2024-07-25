51ee6e6a3380ed5b589bff278c2d9449c6053cef
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-25 16:06:29 +0000

    component updated to use reactive forms

--- src/app/modal/edit-profession-modal/edit-profession-modal.component.ts
+++ src/app/modal/edit-profession-modal/edit-profession-modal.component.ts
@@ -1,21 +1,39 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ProfessionService } from '../../admin/service/profession/profession.service';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-edit-profession-modal',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './edit-profession-modal.component.html',
   styleUrl: './edit-profession-modal.component.scss'
 })
 export class EditProfessionModalComponent {
   @Output() cancel = new EventEmitter<void>()
   @Output() submit = new EventEmitter<void>()
+  public editProfessionForm: FormGroup
+
+  constructor(
+    private toast: NgToastService,
+    private fb: FormBuilder,
+    private ps: ProfessionService) {
+      this.editProfessionForm = this.fb.group({
+        name: ['']
+      })
+    }
 
   onCancel() {
     this.cancel.emit()
   }
 
   onSubmit() {
+    if(this.editProfessionForm.invalid) {
+      return
+    }
+
+    this.ps.editEmergency
     this.submit.emit()
   }
 }
