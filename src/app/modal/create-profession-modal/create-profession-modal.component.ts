10ac159fcaafe4007ed15860e4ed563b81a18da8
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-25 16:05:58 +0000

    profession modal updated to create new profession

--- src/app/modal/create-profession-modal/create-profession-modal.component.ts
+++ src/app/modal/create-profession-modal/create-profession-modal.component.ts
@@ -7,7 +7,6 @@ import { ProfessionService } from '../../admin/service/profession/profession.ser
 import { nameValidator } from '../../validators/nameValidator';
 import { EmergencyService } from '../../admin/service/emergency/emergency.service';
 import { EmergencyType } from '../../model/emergency-types';
-import { CreateProfession } from '../../model/professions';
 
 @Component({
   selector: 'app-create-profession-modal',
@@ -21,63 +20,40 @@ import { CreateProfession } from '../../model/professions';
   templateUrl: './create-profession-modal.component.html',
   styleUrl: './create-profession-modal.component.scss'
 })
-export class CreateProfessionModalComponent implements OnInit{
+export class CreateProfessionModalComponent {
   @Output() cancel = new EventEmitter<void>()
   @Output() submit = new EventEmitter<void>()
   public addProfessionForm: FormGroup
   public loading: boolean = false
-  public emergency: EmergencyType[] = []
 
   constructor(
     private toast: NgToastService,
     private fb: FormBuilder,
-    private ps: ProfessionService,
-    private es: EmergencyService
-  ) {
+    private ps: ProfessionService) {
     this.addProfessionForm = this.fb.group({
-      name: ['', [Validators.required, nameValidator()]],
-      emergencyId: ['', Validators.required]
-    })
-  }
-
-  ngOnInit(): void {
-    this.getEmergency('all')
-  }
-
-  getEmergency(want: string): void {
-    this.es.getEmergencies(undefined, want).subscribe({
-      next: (response) => {
-        this.emergency = response.data.emergencyTypes
-      },
-      error: () => {
-        this.toast.danger("Failed to fetch emergency", "Error", 3000)
-      }
+      name: ['', [Validators.required, nameValidator()]]
     })
   }
 
   onSubmit() {
     if(this.addProfessionForm.invalid) {
-      this.toast.warning('Please fill all the fields', 'Required fields', 3000)
       return
     }
 
     this.loading = true
+    const name: string = this.addProfessionForm.value
 
-    const type: CreateProfession = {
-      ...this.addProfessionForm.value,
-      emergencyId: Number(this.addProfessionForm.value.emergencyId)
-    }
-
-    this.ps.addProfession(type).subscribe({
+    this.ps.addProfession(name).subscribe({
       next: (response) => {
         this.loading = false
         this.toast.success(response.message, "Success", 3000)
-        this.submit.emit()
         this.addProfessionForm.reset()
+        this.submit.emit()
       },
       error: (err) => {
+        console.error('error', err)
         this.loading = false
-        this.toast.danger("Error creating profession: " + (err?.error?.message || 'Unknown error'), "Error", 3000)
+        this.toast.danger((err?.error?.message || 'Error adding profession'), "Error", 3000)
       }
     })
   }
