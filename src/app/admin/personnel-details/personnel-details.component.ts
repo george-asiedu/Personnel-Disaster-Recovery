643a0c16da057bc79d7c10b1a8e2f2d49070a14c
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-26 18:35:31 +0000

    component updated to fetch personnel data

--- src/app/admin/personnel-details/personnel-details.component.ts
+++ src/app/admin/personnel-details/personnel-details.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SuspendModalComponent } from '../../modal/suspend-modal/suspend-modal.component';
 import { NgIf } from '@angular/common';
+import { GetPersonnel, Personnel } from '../../model/profile';
+import { ActivatedRoute } from '@angular/router';
+import { PersonnelService } from '../../personnel/service/personnel.service';
 
 @Component({
   selector: 'app-personnel-details',
@@ -9,21 +12,49 @@ import { NgIf } from '@angular/common';
   templateUrl: './personnel-details.component.html',
   styleUrl: './personnel-details.component.scss'
 })
-export class PersonnelDetailsComponent {
-  isSuspendModalVisible = false;
+export class PersonnelDetailsComponent implements OnInit {
+  isSuspendModalVisible = false
+  public personnel: Personnel | null = null
+  public loading: boolean = true
+
+  constructor(
+    private route: ActivatedRoute,
+    private ps: PersonnelService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    console.log('id', id)
+    if (id) {
+      this.fetchPersonnelDetails(id)
+    }
+  }
+
+  fetchPersonnelDetails(id: string) {
+    this.ps.getPersonnelById(id).subscribe({
+      next: (response) => {
+        console.log('response id', id)
+        
+        this.loading = false
+        this.personnel = response
+      },
+      error: () => {
+        this.loading = false
+      }
+    })
+  }
 
   showSuspendModal() {
-    this.isSuspendModalVisible = true;
+    this.isSuspendModalVisible = true
   }
 
   cancelSuspend() {
-    this.isSuspendModalVisible = false;
+    this.isSuspendModalVisible = false
   }
 
   confirmSuspend() {
-    // Add your suspend logic here
-    this.isSuspendModalVisible = false;
-    alert('Personnel suspended');
+    this.isSuspendModalVisible = false
+    alert('Personnel suspended')
   }
 
 }
