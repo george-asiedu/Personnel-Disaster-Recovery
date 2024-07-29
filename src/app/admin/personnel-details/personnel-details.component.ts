3c05c3c2802a747e59a3c7c9364838be7836f55d
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-29 16:59:19 +0000

    personnel details page component updated

--- src/app/admin/personnel-details/personnel-details.component.ts
+++ src/app/admin/personnel-details/personnel-details.component.ts
@@ -1,60 +1,84 @@
 import { Component, OnInit } from '@angular/core';
-import { SuspendModalComponent } from '../../modal/suspend-modal/suspend-modal.component';
-import { NgIf } from '@angular/common';
-import { GetPersonnel, Personnel } from '../../model/profile';
+import { DatePipe, NgFor, NgIf } from '@angular/common';
+import { PersonnelDetails } from '../../model/profile';
 import { ActivatedRoute } from '@angular/router';
 import { PersonnelService } from '../../personnel/service/personnel.service';
+import { SpinnerComponent } from '../../components/spinner/spinner.component';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-personnel-details',
   standalone: true,
-  imports: [SuspendModalComponent, NgIf],
+  imports: [
+    NgIf, 
+    NgFor, 
+    SpinnerComponent,
+    DatePipe
+  ],
   templateUrl: './personnel-details.component.html',
   styleUrl: './personnel-details.component.scss'
 })
 export class PersonnelDetailsComponent implements OnInit {
-  isSuspendModalVisible = false
-  public personnel: Personnel | null = null
-  public loading: boolean = true
+  public personnel: PersonnelDetails = {} as PersonnelDetails
+  public loading: boolean = false
 
   constructor(
     private route: ActivatedRoute,
-    private ps: PersonnelService
+    private ps: PersonnelService,
+    private toast: NgToastService
   ) {}
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id')
-    console.log('id', id)
-    if (id) {
-      this.fetchPersonnelDetails(id)
-    }
+    this.route.paramMap.subscribe(
+      (param) => {
+        let id: string | null = param.get('id')
+        if(id !== null) this.fetchPersonnelDetails(id)
+      })
   }
 
   fetchPersonnelDetails(id: string) {
+    this.loading = true
     this.ps.getPersonnelById(id).subscribe({
       next: (response) => {
-        console.log('response id', id)
-        
         this.loading = false
         this.personnel = response
+        this.toast.success('Personnel details fetched successfully', 'Success', 3000)
       },
-      error: () => {
+      error: (err) => {
         this.loading = false
+        this.toast.danger(err.error?.message, 'Error', 3000)
       }
     })
   }
 
-  showSuspendModal() {
-    this.isSuspendModalVisible = true
+  verifyPersonnel(id: string): void {
+    this.loading = true
+    this.ps.verifyPersonnel(id).subscribe({
+      next: (response) => {
+        this.loading = false
+        this.toast.success(response.message, 'Success', 3000)
+      }, error: (err) => {
+        this.loading = false
+        this.toast.danger(err.error?.message, 'Error', 3000)
+      }
+    })
   }
 
-  cancelSuspend() {
-    this.isSuspendModalVisible = false
+  deletePersonnel(id: string): void {
+    const confirmDeletion = window.confirm('Are you sure you want to delete this personnel?')
+    if (!confirmDeletion) {
+      return
+    }
+    this.loading = true
+    this.ps.deletePersonnelProfile(id).subscribe({
+      next: (response) => {
+        this.loading = false
+        this.toast.success(response.message, 'Success', 3000)
+      },
+      error: (err) => {
+        this.loading = false
+        this.toast.danger(err.error?.message, 'Error', 3000)
+      }
+    })
   }
-
-  confirmSuspend() {
-    this.isSuspendModalVisible = false
-    alert('Personnel suspended')
-  }
-
 }
