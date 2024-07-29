941e281011ad25b80dc928bd34748fc71e3ab245
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-29 17:00:07 +0000

    assets added

--- src/app/components/table/emergency-initiative-table/emergency-initiative-table.component.ts
+++ src/app/components/table/emergency-initiative-table/emergency-initiative-table.component.ts
@@ -1,5 +1,12 @@
-import { NgClass, NgFor, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { DatePipe, NgClass, NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { PaginationComponent } from '../../pagination/pagination.component';
+import { SpinnerComponent } from '../../spinner/spinner.component';
+import { EmergencyInitiativeService } from '../../../admin/service/emergency-initiative/emergency-initiative.service';
+import { NgToastService } from 'ng-angular-popup';
+import { Initiative } from '../../../model/emergency-initiative';
+import { AuthService } from '../../../auth/authService/auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-emergency-initiative-table',
@@ -7,29 +14,78 @@ import { Component } from '@angular/core';
   imports: [
     NgFor,
     NgClass,
-    NgIf
+    NgIf,
+    PaginationComponent,
+    SpinnerComponent,
+    DatePipe,
+    RouterLink
   ],
   templateUrl: './emergency-initiative-table.component.html',
   styleUrl: './emergency-initiative-table.component.scss'
 })
-export class EmergencyInitiativeTableComponent {
-  public isEditModalOpen: boolean = false
+export class EmergencyInitiativeTableComponent implements OnInit {
+  public loading: boolean = false
+  public initiative: Initiative[] = []
+  public isDropdownVisible: boolean[] = []
+  public totalInitiative: number = 0
+  public currentPage: number = 0 
+  public pageSize: number = 9
+  public role: string = ''
+  
+  constructor(
+    private toast: NgToastService,
+    private eis: EmergencyInitiativeService,
+    private as: AuthService
+  ) {}
 
-  public emergencies = [
-    { managerName: 'George Asiedu', emergencyType: 'Security threat', date: 'May 9, 2024', location: '2 PINE STREET' },
-    { managerName: 'George Asiedu', emergencyType: 'Pandemic', date: 'May 9, 2024', location: '2 PINE STREET' },
-    { managerName: 'George Asiedu', emergencyType: 'Epidemic outbreaks', date: 'May 9, 2024', location: '2 PINE STREET' },
-    { managerName: 'George Asiedu', emergencyType: 'Civil unrest', date: 'May 9, 2024', location: '2 PINE STREET' },
-    { managerName: 'George Asiedu', emergencyType: 'Wildfire', date: 'May 9, 2024', location: '2 PINE STREET' },
-    { managerName: 'George Asiedu', emergencyType: 'Health emergency', date: 'May 9, 2024', location: '2 PINE STREET' },
-    { managerName: 'George Asiedu', emergencyType: 'Flood', date: 'May 9, 2024', location: '2 PINE STREET' },
-    { managerName: 'George Asiedu', emergencyType: 'Industrial accident', date: 'May 9, 2024', location: '2 PINE STREET' },
-    { managerName: 'George Asiedu', emergencyType: 'Infrastructural failure', date: 'May 9, 2024', location: '2 PINE STREET' }
-  ]
+  ngOnInit(): void {
+    this.getEmergencyInitiative(this.currentPage) 
+    this.getUserRole()
+  }
 
-  public isDropdownVisible: boolean[] = Array(this.emergencies.length).fill(false)
+  getEmergencyInitiative(page: number): void {
+    this.loading = true
+    this.eis.getEmergencyInitiatives(page).subscribe({
+      next: (response) => {
+        this.loading = false
+        this.initiative = response.data.initiatives
+        this.totalInitiative = response.data.count
+        this.isDropdownVisible = Array(this.initiative.length).fill(false)
+      }, error: (err) => {
+        this.loading = false
+        this.toast.danger(err.error?.message, 'Error', 3000)
+      }
+    })
+  }
 
   toggleDropdown(index: number) {
-    this.isDropdownVisible = this.isDropdownVisible.map((visible, i) => (i === index ? !visible : false))
+    this.isDropdownVisible = this.isDropdownVisible.map((visible, i) =>
+      i === index ? !visible : false
+    )
+  }
+
+  handlePageChange(page: number): void {
+    this.currentPage = page
+    this.getEmergencyInitiative(this.currentPage + 1)
+  }
+
+  getUserRole(): void {
+    const currentUser = this.as.currentUserValue
+    if(currentUser) {
+      const userRole = currentUser.data.user.role
+      this.role = userRole  
+    }
+  }
+
+  approveEmergency(id: string): void {
+    this.eis.approveEmergencyInitiative(id).subscribe({
+      next: (response) => {
+        this.toast.success(response.message, 'Success', 3000)
+        this.getEmergencyInitiative(this.currentPage)
+      },
+      error: (err) => {
+        this.toast.danger(err.error?.message, 'Error', 3000)
+      }
+    });
   }
 }
