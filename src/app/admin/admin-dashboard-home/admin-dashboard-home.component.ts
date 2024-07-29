a51ff7f9d7c40cbf912e9d48ed02bb163178573a
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-07-29 16:53:55 +0000

    admin home dashboard page updated to count total activities done by the admin

--- src/app/admin/admin-dashboard-home/admin-dashboard-home.component.ts
+++ src/app/admin/admin-dashboard-home/admin-dashboard-home.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth/authService/auth.service';
+import { PersonnelService } from '../../personnel/service/personnel.service';
+import { EmergencyService } from '../service/emergency/emergency.service';
+import { ManagerService } from '../service/manager/manager.service';
+import { ProfessionService } from '../service/profession/profession.service';
 
 @Component({
   selector: 'app-admin-dashboard-home',
@@ -7,6 +12,62 @@ import { Component } from '@angular/core';
   templateUrl: './admin-dashboard-home.component.html',
   styleUrl: './admin-dashboard-home.component.scss'
 })
-export class AdminDashboardHomeComponent {
+export class AdminDashboardHomeComponent implements OnInit {
+  public name: string | null = null
+  public totalRegisteredPersonnel: number = 0
+  public totalEmergenciesData: number = 0
+  public totalVerifiedPersonnel: number = 0
+  public totalManagers: number = 0
+  public totalProfessions: number = 0
 
+  constructor(
+    private ps: PersonnelService,
+    private es: EmergencyService,
+    private mn: ManagerService,
+    private prof: ProfessionService,
+    private authService: AuthService) {}
+  
+  ngOnInit(): void {
+    const currentUser = this.authService.currentUserValue
+    if (currentUser) {
+      const user = currentUser.data.user
+      this.name = user.name
+    }
+    this.personnelData()
+    this.totalEmergencies()
+    this.professionsTotal()
+    this.managersTotal()
+  }
+
+  personnelData(): void {
+    this.ps.getPersonnelData(0).subscribe({
+      next: (response) => {
+        this.totalRegisteredPersonnel = response.count
+        const verified = response.personnel.filter(state =>
+          state.status === 'APPROVE' 
+        )
+        this.totalVerifiedPersonnel = verified.length
+      }
+    })
+  }
+
+  totalEmergencies(): void {
+    this.es.getEmergencies(0).subscribe({
+      next: (response) => {
+        this.totalEmergenciesData = response.data.count
+      }
+    })
+  }
+
+  professionsTotal(): void {
+    this.prof.getProfession(0).subscribe(response => {
+      this.totalProfessions = response.data.count
+    })
+  }
+
+  managersTotal(): void {
+    this.mn.getManagers(0).subscribe(response => {
+      this.totalManagers = response.data.count
+    })
+  }
 }
